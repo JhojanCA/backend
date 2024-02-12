024f2514f20190af8fb127b11c3df811bd9b21c0
JhojanCA <jhojanalexca12@gmail.com>
2024-02-11 22:15:09 -0500

    Se subió la BD a la nube y se colocaron sus datos en las variables de entorno

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('almacen', 'root', '', {
-    host: 'localhost',
+/* const sequelize = new Sequelize('almacen', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+}); */
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('almacen', 'root', '', {
+/* const sequelize = new Sequelize('almacen', 'root', '', {
   host: 'localhost',
+  dialect: 'mysql'
+}); */
+
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
